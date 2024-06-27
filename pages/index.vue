09ab7634e1e41bdbb6193e50979c77f7fa43920f
JuninhoFreitas <joaofbjunior01@gmail.com>
2024-06-26 23:01:09 -0400

    feat: add nuxt

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div class="p-5 h-screen bg-gray-900 flex items-center justify-center">
+    <!-- Esse é um site para montar uma lista de itens faltando no estoque
+    Deverá conter uma lista de itens, separado por categorias
+    Quando um item for selecionado, deverá ser adicionado a uma lista de itens faltando
+    A lista de itens faltando deverá ser exibida em um modal
+    Deverá ser possível remover um item da lista de itens faltando
+    Deverá ser possível adicionar um item a lista de itens faltando
+    Deverá ser possível editar a quantidade de um item na lista de itens faltando através de botões de + e -
+    Os botões de adicionar ou remover também terão +10 e -10
+    Deverá ser possível adicionar um item a lista de itens faltando através de um input de texto
+    Deverá ser possível editar a quantidade de um item na lista de itens faltando através de um input de texto
+    Deverá ser possível adicionar um item a lista de itens faltando através de um leitor de código de barras
+    Após adicionar um item a lista de itens faltando, o input de texto deverá ser limpo
+    Após concluído, deverá enviar a lista de itens faltando para um endpoint 
+    Utilize a biblioteca PrimeVue para os componentes visuais
+    Utilize a biblioteca Axios para as requisições HTTP
+    Se for necessário criar 
+    -->
+  </div>
+</template>
